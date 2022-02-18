8364c2acfaab050f256cd5846dcc08a60473a29d
logicalOak <mozhnyy.dev@gmail.com>
2022-02-18 17:46:01 +0300

    added quiz project

---
+++ src/components/Question.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Question = ({
+	data,
+	onAnswerUpdate,
+	numberOfQuestions,
+	activeQuestion,
+	onSetActiveQuestion,
+	onSetStep,
+}) => {
+	const [selected, setSelected] = useState('');
+	const [error, setError] = useState('');
+	const radiosWrapper = useRef();
+
+	useEffect(() => {
+		const findCheckedInput =
+			radiosWrapper.current.querySelector('input:checked');
+		if (findCheckedInput) {
+			findCheckedInput.checked = false;
+		}
+	}, [data]);
+
+	const changeHadler = (e) => {
+		setSelected(e.target.value);
+		if (error) {
+			setError('');
+		}
+	};
+
+	const nextClickHandler = (e) => {
+		if (selected === '') {
+			return setError('Please selected one option');
+		}
+		onAnswerUpdate((prevState) => [
+			...prevState,
+			{ q: data.question, a: selected },
+		]);
+		setSelected('');
+		if (activeQuestion < numberOfQuestions - 1) {
+			onSetActiveQuestion(activeQuestion + 1);
+		} else {
+			onSetStep(3);
+		}
+	};
+
+	return (
+		<div className='card'>
+			<div className='card-content'>
+				<div className='content'>
+					<h2 className='mb-5'>{data.question}</h2>
+					<div className='control' ref={radiosWrapper}>
+						{data.choices.map((choice, i) => (
+							<label
+								className='radio has-background-light'
+								key={i}
+							>
+								<input
+									type='radio'
+									name='answer'
+									value={choice}
+									onChange={changeHadler}
+								/>
+								{choice}
+							</label>
+						))}
+					</div>
+					{error && <div className='has-text-danger'>{error}</div>}
+					<button
+						className='button is-link is-medium is-fullwidth mt-4'
+						onClick={nextClickHandler}
+					>
+						Next
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Question;
